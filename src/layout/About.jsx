5bd53f39200840c1ad3c0aeb058e6cb690e51520
panza123 <115466750+panza123@users.noreply.github.com>
2024-03-06 03:33:53 -0800

    Update About.jsx

--- src/layout/About.jsx
+++ src/layout/About.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import logo from '../assets/image/house2.png'
 export default function About() {
   return (
-    <main className='max-w-[1440px] px-5 grid md:grid-cols-2 gap-5 '>
+    <main className='max-w-[1440px] px-5 flex flex-col md:grid grid-cols-2 gap-5 '>
 
 <div className='mt-10 '> 
        <img src={logo} alt=""  className='object-cover rounded-md'/>
     </div>
 
     <div >
-       <h3 className=' text-4xl md:text-5xl lg:text-7xl font-bold text-[#085C11E8]'>
+       <h3 className=' text-4xl md:text-5xl lg:text-7xl font-bold text-[#085C11E8] order-2'>
        Begin your awesome journey
        </h3>
-       <p className='pt-10 md:text-[15px]'>
+       <p className='pt-10 md:text-[15px] order-1'>
        Looking for the perfect home to rent? Our user-friendly house rental website makes your search a breeze. 
 Browse through a diverse range of listings,
 From cozy apartments to spacious family houses. </p>
